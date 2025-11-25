d5ac97a66d44cedafd3b8eb5bf1d479803e14a1b
kallem Ullah <kohatiii006@gmail.com>
2025-11-25 11:50:50 +0500

    J.S Assignment 01

---
+++ J.S Class 01/app.js
@@ -0,0 +1,121 @@
+// alert("pakistan");
+
+
+// var myName = 25 ;
+
+// var greetings = "hello how are you";
+
+// alert (greetings)
+
+// var a = 2;
+// var b =4 ;
+
+// console.log(a,b)
+
+//  alert question section..
+
+// CHAPTER 1 ALERT ........................................................
+
+// ..... (Q1) .......................................
+
+// alert("Error ! please enter a valid password");
+
+// ..... (Q2) .....
+
+// alert("welcome to js Land...\nHappy Coding!");
+
+// ..... (Q3) .....
+
+// alert("WELCOME TO JS LAND..")
+
+// ..... (Q4) .....
+
+// alert("HAPPY CODING !")
+
+// ..... (Q5) .....
+
+// alert("hello... i can run js through my web browser's console")
+
+// ..... (Q6) .....
+
+
+
+// CHAPTER 2  VARIABLES FOR STRING ............................................
+
+//  variable question section.........................
+
+// bio data ...................................................
+
+// var myName = "myName : kaleem ullah";
+
+// alert (myName);
+
+// var myAge = "myAge : 25 years old";
+
+// alert (myAge);
+
+// var myCourse = (" myCourse : certified web developer")
+
+// alert (myCourse);
+
+// var myBioData = ("my bio data ; i am kaleem ullah from Karachi , and i am 25 years old , i am certified web developer")
+
+// alert(myBioData)
+
+// pizzzaaaaaaaaaaaaaaaaaa .............................
+
+// var pizza = ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// alert (pizza)
+
+
+// Email ...................................................
+
+// var email = ("myemail : kohatiii006@gmail.com")
+
+// alert (email)
+
+// smarter way .............................................
+
+// var smarterWay = ("i am traying to lear js from the book \n a smarter way to learn js")
+
+// alert (smarterWay)
+
+// document.write .........................................
+
+// var document1 = ("yah! i can write HTML content through javascript")
+
+// console.log (document1)
+
+
+
+// CHAPTER 3  VARIABLES FOR NUMBERS  ............................................
+
+//  variable question section.........................
+
+// age ...................................................  
+
+
+// var age = (" i am 25 years old")
+
+// alert (age)
+
+//  visists ...................................................
+
+// var visits = (" you have visisted 14 times this site")
+
+// alert (visits)
+
+// birth year ...................................................
+
+// var birthYear = ("my birth year is 1995 \n Data type declared is variable number")
+// alert (birthYear)
+
+// decoment.write .........................................
+
+document.write("my birth year is 1995 <br> Data type declared is variable number");  
+
+doc
+
+
+
