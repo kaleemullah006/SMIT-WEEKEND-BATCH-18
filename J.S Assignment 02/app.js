33060d2545b40510ccaab433bcb718141160118f
kaleem Ullah <kohatiii006@gmail.com>
2025-12-06 13:19:17 +0500

    J.S Assignment 02

---
+++ J.S Assignment 02/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+var currentAge = +prompt("enter your currentAge");
+var futureAge = +prompt("enter your futureage")
+
+var currentAgeInNumber = (currentAge)
+var futureAgeInNumber = (futureAge)
+
+var futureAgeAfterTenYear= currentAge + futureAge;
+
+console.log(futureAgeAfterTenYear)
