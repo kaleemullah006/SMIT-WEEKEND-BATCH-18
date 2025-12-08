770d4ce67139b43ec1b0960a8180c7ce8896407f
kallem Ullah <kohatiii006@gmail.com>
2025-12-09 00:48:46 +0500

    js class 03 practise session 1

---
+++ j.s class 03 practise/app.js
@@ -0,0 +1,52 @@
+// Q1 
+
+//  ALERT 
+
+// var name = prompt("Enter Your Name")
+// var age = prompt("Enter Your Age")
+// var city = prompt("Enter Your City")
+
+
+// alert("welcome"+ " " + name)
+// alert (`you are `+ age + ` years old`)
+// alert ("You Live in " + city)
+
+// IF / ELSE
+
+// var age = prompt("Enter Your Age");
+
+// if (age >= 18){
+//     alert ("You Are Eligible For Vote")
+// }
+// else {
+//     alert("You Are Noyt Eligible For Vote")
+// }
+
+// Q2
+
+// var value = prompt("Enter Your Value")
+
+// if (value > 0){
+//     alert("positive")
+// }
+// if (value < 0){
+//     alert("negative")
+// }
+// if (value == 0){
+//     alert("zero")
+// }
+
+// Q3 
+
+// var userName = prompt("Enter Your UserName")
+// var password = prompt("Enter Your Password")
+
+// if(userName === "admin" && password === "12345"){
+//     alert("Login Successful")
+// }
+// else {
+//     alert ("Invalid Credentials")
+// }
+
+
+
