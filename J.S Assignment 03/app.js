191fb0fe13c1388bba71345800a34eac34a5dfdf
kallem Ullah <kohatiii006@gmail.com>
2025-12-12 23:05:43 +0500

    Js Assignment 03

---
+++ J.S Assignment 03/app.js
@@ -0,0 +1,49 @@
+// Assignment 03 
+
+// var familyMembers = [];
+
+// var member1 = prompt("add family memeber 1")
+// familyMembers.push(member1)
+// var member2 = prompt("add family memeber 2")
+// familyMembers.push(member2)
+// var member3 = prompt("add family memeber 3")
+// familyMembers.push(member3)
+// var member4 = prompt("add family memeber 4")
+// familyMembers.push(member4)
+// var member5 = prompt("add family memeber 5")
+// familyMembers.push(member5)
+// var member6 = prompt("add family memeber 6")
+// familyMembers.push(member6)
+// var member7 = prompt("add family memeber 7")
+// familyMembers.push(member7)
+// var member8 = prompt("add family memeber 8")
+// familyMembers.push(member8)
+// var member9 = prompt("add family memeber 9")
+// familyMembers.push(member9)
+// var member10 = prompt("add family memeber 10")
+// familyMembers.push(member10)
+
+// console.log(familyMembers)
+
+var familyMembers = [];
+var member1 = prompt("Enter member 1 name")
+familyMembers.push(member1);
+ var member2 = prompt("Enter member 2 name")
+ familyMembers.push(member2);
+var member3 = prompt("Enter memeber 3 name")
+familyMembers.push(member3);
+var member4 = prompt("Enter member 4 name ")
+familyMembers.push(member4)
+var member5  = prompt("Enter member 5 name ")
+familyMembers.push(member5)
+var member6 = prompt("Enter member 6 name")
+familyMembers.push(member6)
+var member7 = prompt("Enter member 7 name")
+familyMembers.push(member7)
+var member8 = prompt("Enter member 8 name")
+familyMembers.push(member8)
+var member9 = prompt("Enter member 9 name")
+familyMembers.push(member9)
+var member10 = prompt("Enter member 10 name")
+familyMembers.push(member10);
+ console.log(familyMembers)
