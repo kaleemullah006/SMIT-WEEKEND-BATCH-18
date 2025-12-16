dbea7771ba9ba91d4dc88d1cdd542b7a622c0611
kallem Ullah <kohatiii006@gmail.com>
2025-12-16 15:08:57 +0500

    js class 04

---
+++ js class 04/app.js
@@ -0,0 +1,40 @@
+// //  splice ....
+
+// var students = ["ahmad", "ali", "danial"]
+// students.splice(1,1,"ahmad")
+// pehla number index btana hai 
+// dosra number ginti btani hai k ketne delete krne hain 
+// or tesra add krna hai
+
+// undefined or not defined alag alag error hai
+
+// // slice .......
+
+// students.slice (1,4)
+// matlab index 1 se 4 se pehle tak length btaye yani alag karden in elemnts ko
+//  or ye mutate nhi karta sirf nayi copy bnata hai asal elemnts ko chnge nhi krta
+
+// //   for loop .........
+
+// keyword for + () + {} ye formula hai
+
+// formula 
+// for (let start  = 26; start < 30 condition; start + 1 update){
+// }
+
+// i ka matlab itration hai 
+
+// for (let i  = 0; i<10; i++){
+//     console.log("hi",i)
+// }
+
+
+// while loop .....
+
+// while (true){
+//     console.log("hello world")
+// }
+
+
+// do while loop .....
+
