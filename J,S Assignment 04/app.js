78b707a6f90498608779ef79b96460820a26d86d
kallem Ullah <kohatiii006@gmail.com>
2025-12-18 13:18:48 +0500

    Js Assignment 04

---
+++ J,S Assignment 04/app.js
@@ -0,0 +1,19 @@
+// ASSIGNMENT 04 - JAVASCRIPT TABLE OF NUMBERS 
+
+
+// table 01 ....
+
+// let number = 2;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i + "x" + number + "=" + (number * i))
+
+// }
+
+
+// table 02 ....
+
+let num = prompt("Enter a number to show its multiplication table");
+for (let i = 1; i <= 10; i++){
+    document.write(i +"x"+ num + "="+(num*i)+"<br>")
+}
