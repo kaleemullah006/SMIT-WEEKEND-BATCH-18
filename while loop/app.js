bfc2a8087913f28e74aef76075d3784384e2da4f
kallem Ullah <kohatiii006@gmail.com>
2025-12-20 00:22:54 +0500

    while loop practice

---
+++ while loop/app.js
@@ -0,0 +1,112 @@
+
+
+// while loop example:.......print numbers from 1 to 5
+
+// let i = 1; // initialization
+
+// while(i <= 5){ // condition
+//     console.log(i); // print the value of i
+//     i++;
+// } // increment
+// Output:
+// 1
+// 2    
+// 3
+// 4
+// 5    
+
+// while loop example:.......table of 2
+
+// let num =2 ;
+
+// let i = 1;
+//  while (i<= 10){
+//     console.log(i+ " x "+ num + " = "+ (i* num))
+//     i++
+//  }
+// //  console.log(i)
+
+
+// practice Questions .......
+
+// Q1 .. .. print numbers from 1 to 10
+
+// let i = 1 ; 
+
+// while (i <= 10 ){
+//     console.log(i)
+//     i++
+// }
+
+// Q1 ... table of 2 
+
+// let num = prompt("Enter the number to print its table: ");
+
+// let i = 1 ;
+
+// while(i <= 10){
+//     console.log(i+" x "+ num + " =  "+(i * num))
+//     i++
+// }
+
+// Q 3 ... condition based
+
+// let number ;
+
+// while ( number !== 0 ){
+//    number = +prompt("enter a number ");
+
+//     console.log(number)
+
+
+    
+// }
+
+// Q4 ... real life logic ....
+
+// battery = 100 ;
+
+// while (battery < 10/100){
+//     console.log(battery + "% remaining")
+//     battery -= 10 ;
+
+// let battery = 100;
+
+// while (battery >= 0) {
+//     console.log("Battery: " + battery + "%");
+//     battery -= 10;
+// }
+
+
+// var name = prompt("Enter your name: ").toLowerCase();
+// var age = prompt("Enter your age: ").toLowerCase();
+
+// document.write(" Your Name is : " + name + "<br>" + "Your Age is: "+ age);
+
+// Q1 ... 
+
+
+// let evenNumber = 0;
+
+// while (evenNumber < 20){
+//     evenNumber += 2;
+//     console.log(evenNumber)
+// }
+
+// Q2 .. 
+
+// let sum = 0;
+// for (let i = 1; i <=10 ; i ++){
+//     sum += i;
+//     console.log("sum after adding "+i+ " is:"+sum)
+
+// }
+  // let sum = 0 ; 
+
+  // for (let i = 0 ; i <= 10 ; i++){
+  //   sum += i;
+
+  //   console.log(i + i+ " = " + sum )
+  // }
+
+
