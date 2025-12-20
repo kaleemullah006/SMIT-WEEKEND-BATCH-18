4150e97805105efcf9b4f838d0df30355bdd1175
kallem Ullah <kohatiii006@gmail.com>
2025-12-20 21:13:46 +0500

    do while loop

---
+++ do while loop/app.js
@@ -0,0 +1,86 @@
+
+
+// do while loop ... 
+
+// let i = 1 ; // initialization hoga
+
+// do {
+//     console.log(i); / i ki value print hoga
+//     i++; // increment
+// } while (i <= 5); // condition check hoga
+
+// Output:  
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// example 2 ; ..... table of 3 
+
+// let num  = 3;
+// let i = 1 ;
+// do {
+//     console.log(i +"x"+ num + "="+ (i * num))
+//     i++;
+// }
+// while (i <= 10);
+
+// example 3 ... 
+
+// let i = 10 
+
+// do {
+//     console.log(i);
+//     i--;
+
+// }while(i>=1)
+
+// let i = 20 ;
+
+// do {
+//     console.log(i);
+//     i--;
+// } while(i >= 1)
+
+// let num =3 ;
+// let i = 1; 
+// do {
+//     console.log( i +" x " + num+" = "+(num*i))
+//     i++;
+// }while (i <=10 );
+//  Q1..  
+// let i = 1 ;
+
+// do {
+//     console.log(i);
+//     i++
+// }while (i <=10)
+
+// Q2 ... 
+
+// let num = 5 ;
+// let i = 1 ;
+// do{
+//     console.log(i + " x " + num+ " = "+ (num*i))
+//     i++;
+// }while(i <=10)
+
+// Q3 ... 
+
+// let i = 10 ;
+
+// do {
+//     console.log(i)
+//     i--;
+// }while(i >=1)
+
+// Q4 ... 
+
+let number = prompt("Enter a number:");
+let i ;
+do {
+    
+    console.log("valid number")
+    i++;
+}while(i >= 0)
