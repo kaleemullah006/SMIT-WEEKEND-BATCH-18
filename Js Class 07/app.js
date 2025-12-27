152a1d7b59ef530beb48adf7e00d5e1f855cdac2
kallem Ullah <kohatiii006@gmail.com>
2025-12-28 00:41:18 +0500

    js class 07

---
+++ Js Class 07/app.js
@@ -0,0 +1,14 @@
+
+// rounding number //
+//  assignment 07
+//  USER K 5 rope bach gaye to chococlete wapis krna hai agar 12 rope ho to 2 choco or 2 rope 
+
+// RANDOM NUMBER   //
+
+
+let random = Math.random( )
+
+console.log(random)
+
+
+to exponent  check karna hai
