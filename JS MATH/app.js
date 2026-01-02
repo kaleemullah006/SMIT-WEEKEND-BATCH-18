fb20aed78023e039e8f54a6d2204dc6da9d54be8
kallem Ullah <kohatiii006@gmail.com>
2026-01-02 11:47:16 +0500

    js math

---
+++ JS MATH/app.js
@@ -0,0 +1,25 @@
+// let random = Math.random();
+
+// console.log(random)
+
+
+
+// let randomMultiply = Math.random()*8;
+
+// console.log(randomMultiply)
+
+// let randomRound = Math.round(Math.random()*19);
+
+// console.log(randomRound)
+
+// number to string 
+
+// let number = 123456;
+
+// console.log(typeof  number.toString(), number)
+
+// decimal length fixed 
+
+let num = 2.4634785734;
+
+console.log(num.toFixed(6))
