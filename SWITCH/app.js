10430ae95541e2f6c0cd52a2cfba746e3fb31204
kallem Ullah <kohatiii006@gmail.com>
2026-01-11 00:37:03 +0500

    SWITCH

---
+++ SWITCH/app.js
@@ -0,0 +1,72 @@
+// SWITCH 
+
+// const day = "thursday";
+
+// switch (day){
+//     case "wednesday":
+//     alert ("continuo")
+//     // break;
+//     case "thursday":
+//         alert ("keep continuo")
+//         // break;
+//         case "sunday":
+//             alert ("keep")
+//             // break;
+
+//             case "friday":
+//                 alert("mil gaya")
+//                 // break;
+
+//                 default: 
+//                 alert("nhi mila")
+// }
+
+// const number = 4;
+
+// switch (number) {
+//     case 1: 
+//         console.log("one")
+//         break;
+
+//     case 2:  
+//         console.log("two")
+//         break;
+
+//     case 3: 
+//         console.log("three")
+//         break;
+
+//     case 4: 
+//         console.log("four")
+//         break;
+
+//     case 5: 
+//         console.log("five")
+//         break;
+//     case 6: 
+//         console.log("six")
+//         break;
+
+//     default:
+//         console.log("invalid")
+// }
+
+// let signal = "yellow";
+
+// switch (signal) {
+//     case "red":
+//         console.log("Stop");
+//         break;
+
+
+//     case "yellow":
+//         console.log("target  fineded")
+//         break;
+
+//     case "green":
+//         console.log("Go");
+//         break;
+
+//     default:
+//         console.log("Invalid signal");
+// }
