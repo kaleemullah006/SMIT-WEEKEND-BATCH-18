b7d8c2570ee87d79047e66510dab30a6d5e5836f
kallem Ullah <kohatiii006@gmail.com>
2026-01-17 15:07:52 +0500

    js events 17/01/2026

---
+++ JS EVENTS PRACTICE/app.js
@@ -0,0 +1,60 @@
+// let btn = document.getElementById("btn")
+// btn.addEventListener("click" , function(){
+//     let message =document.getElementById("text")
+
+//     let isHide = message.classList.contains("hide")
+
+//     if(isHide){
+//         message.classList.replace("hide","show")
+//         btn.innerText ="Hide"
+//     }else{
+//         message.classList.replace("show","hide")
+//         btn.innerText ="Show"
+
+//     }
+// })
+
+// let btn = document.getElementById("btn")
+// btn.addEventListener("click" , function(){
+//     let box= document.getElementById("box")
+
+//     let isRed = box.classList.contains("red")
+
+//     if(isRed){
+//         box.classList.replace ("red", "green")
+//         btn.innerText= "red karden"
+//     }else{
+//           box.classList.replace ("green", "red")
+//         btn.innerText= "green karden"
+//     }
+// })
+
+
+// let btn = document.getElementById("btn")
+// btn.addEventListener("click" , function(){
+//     let box= document.getElementById("bulb")
+
+//     let isOff = box.classList.contains("off")
+// if(isOff){
+//     box.classList.replace("off" , "on")
+// }
+//     else{
+//     box.classList.replace("on" , "off")
+
+//     }
+// })
+
+
+// let btn = document.getElementById("btn")
+// btn.addEventListener("click", function () {
+//     let password = document.getElementById("pass")
+
+//     let isHide = password.type === "password"
+//     if (isHide) {
+//         password.type = "text"
+//         btn.innerText = "hide karden"
+//     } else {
+//         password.type = "password"
+//         btn.innerText = "show karden"
+//     }
+// })
