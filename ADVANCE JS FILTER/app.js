b98b325ebb68466bd94034e4b360cc29afe4a4bb
Kaleem Ullah <kohatiii006@gmail.com>
2026-02-15 00:24:29 +0500

    advance REDUCE

---
+++ ADVANCE JS FILTER/app.js
@@ -0,0 +1,62 @@
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// let result =numbers.filter(number=> number % 2 === 0)
+
+// console.log(result)
+
+
+// const marks = [40, 75, 30, 90, 55];
+
+// let result =marks.filter(mark => mark >= 50)
+
+// console.log(result)
+
+// const players = [
+//   { name: "Ali", score: 45 },
+//   { name: "Ahmed", score: 90 },
+//   { name: "John", score: 60 },
+//   { name: "Bashir", score: 30 },
+//   { name: "Hassan", score: 75 }
+// ];
+
+// let result =players.filter(player => player.score >= 60)
+
+// console.log(result)
+
+// const numbers = [5, 9, 12, 15, 18];
+
+// let even = numbers.find(number => number % 2 === 0 )
+
+// console.log(even)
+
+// const prices = [100, 150, 250, 80, 300];
+
+// let result = prices.find(price => price > 200 )
+
+// console.log(result)
+
+// const students = [
+//   { name: "Ali", marks: 40 },
+//   { name: "Ahmed", marks: 30 },
+//   { name: "John", marks: 55 },
+//   { name: "Bashir", marks: 80 }
+// ];
+
+
+// let result = students.find(student => student.marks >= 50 )
+
+// console.log(result)
+
+// const students = [
+//   { name: "Ali", marks: 40 },
+//   { name: "Ahmed", marks: 30 },
+//   { name: "John", marks: 55 },
+//   { name: "Bashir", marks: 80 }
+// ];
+
+
+// let result = students.filter(student => student.marks >= 50 )
+
+// console.log(result)
+
---
+++ ADVANCE JS REDUCE/app.js
@@ -0,0 +1,83 @@
+// const marks = [40, 60, 80, 20];
+
+// let result = marks.reduce((sum , num) => sum + num, 0)
+
+// console.log(result)
+
+// const marks = [40, 60, 80, 20];
+
+// let result = marks.reduce((sum,num) => sum + num  )
+
+// let average = result/marks.length
+
+// console.log("Total:", result)
+// console.log("average:", average)
+
+// let numbers = [10, 20, 30, 40, 50]
+
+// let total = numbers.reduce((sum,num) => sum + num)
+
+// let average = total/ numbers.length
+
+// console.log("total :" , total)
+// console.log("average :" , average)
+
+// let numbers = [5, 12, 8, 20, 3]
+
+// let highest = numbers.reduce((number , max) => {
+//     if (number > max){
+//         return number
+//     }
+//     else{
+//         return max
+//     }
+// })
+// console.log(highest)
+
+
+// let numbers = [5, 12, 8, 20, 3]
+
+// let highest = numbers.reduce((number , max) => number > max ? number : max
+   
+// )
+// console.log(highest)
+
+// let numbers = [15, 7, 22, 3, 10]
+
+// let lowest = numbers.reduce((min, number) => number < min ? number : min)
+// console.log(lowest)
+
+// let numbers = [5, 10, 15, 20, 25, 30]
+
+// let result = numbers.reduce((even, num) => {
+//     if(num % 2 === 0){
+
+//         return even + num
+//     }
+//     else{
+//     return even
+//     }
+// } )
+
+// console.log(result)
+
+// const arr = [1,2,3,4,5];
+
+// arr.reverse()
+// console.log(arr)
+
+// const arr = [1,2,3,4,5];
+
+// let reverceNumber= arr.toReversed()
+// console.log(arr,"original")
+// console.log(reverceNumber,"copy")
+
+// const arr = [1,2,3,4,5];
+
+// let even = arr.every(num => num % 2 === 0)
+// console.log(even)
+
+const arr = [1,2,3,4,5];
+let fill = arr.fill(9,0,3)
+console.log(fill)
+
