8ff401bff6ded321be45bd9ead89ab51178229d7
Kaleem Ullah <kohatiii006@gmail.com>
2026-04-04 00:18:12 +0500

    ADVANCE METHOD OF ARRAY

---
+++ ADVANCE METHOD OF ARRAY/app.js
@@ -0,0 +1,87 @@
+// let arr = [2,4,6]
+
+// arr.forEach(function(num){
+//     
+//     console.log(num + num)
+
+// })
+
+// let arr = [1,2,3]
+
+// let result = arr.map(function(num){
+//   return num + 5
+// })
+
+// console.log(result)
+
+// let arr = [1,2,3,4,5]
+
+// let result = arr.filter(function(num){
+//    return num > 3
+// })
+
+// console.log(result)
+
+// let arr = [2,4,6]
+
+// arr.forEach(function(num){
+//     
+//     console.log(num + num)
+
+// })
+
+// let arr = [1,2,3]
+
+// let result = arr.map(function(num){
+//   return num + 5
+// })
+
+// console.log(result)
+
+
+
+// let arr = [5,10,10,15,20]
+
+// let result = arr.find(function(num){
+//    return num > 12
+// })
+
+// console.log(result)
+
+
+// let arr = [1,2,[3,[4,5]]]
+
+// let result = arr.flat(1)
+
+// console.log(result)
+
+// let arr = [1,[2,3],[4,5]]
+
+// let result = arr.flat()
+
+// console.log(result)
+
+// 
+
+// class car {
+//     constructor(name){
+//         this.name= name
+//     }
+// }
+
+// let c1 = new car ("Honda")
+
+// console.log(c1)
+
+// class user {
+//     constructor(name,age){
+//         this.name= name
+//         this.age = age
+//     }
+// }
+
+// let user1 = new user ("ahmad", 25)
+
+// console.log(user1.name)
+// console.log(user1.age)
+
