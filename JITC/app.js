70c6834f7399d945116211475fd9ad8ec33186c1
Kaleem Ullah <kohatiii006@gmail.com>
2026-04-07 16:43:29 +0500

    iterator

---
+++ JITC/app.js
@@ -0,0 +1,57 @@
+// function jitTest(){
+//     var x = 5
+//     let y = 10
+//     const z = 15
+//     console.log(x)
+//     console.log(y)
+//     console.log(z)
+// }
+
+// jitTest()
+
+// let greet = "hello"
+
+// function sayHello(){
+//     let name = "haniya"
+//     console.log(greet + " " + name)
+// }
+
+// sayHello()
+// console.log(greet)
+// console.log(name)
+
+
+// function a(){
+//    setTimeout(() => {
+    
+//        console.log("A")
+//    }, 2000); 
+// }
+// function b (){
+//     setTimeout(() => {
+//         a()
+        
+//         console.log("B")
+//     }, 2000);
+// }
+// function c(){
+//     setTimeout(() => {
+//        b()
+    
+//        console.log("C")
+//    }, 2000); 
+// }
+// setTimeout(() => {
+    
+//     c()
+// },2000);
+
+// let arr = ["apple", "banana"]
+
+// let newArr = arr[Symbol.iterator]()
+
+// console.log(newArr.next())
+// console.log(newArr.next())
+// console.log(newArr.next())
+
+
