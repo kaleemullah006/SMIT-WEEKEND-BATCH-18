5d584339ed70349f12c769efeab1c063bcfa8094
Kaleem Ullah <kohatiii006@gmail.com>
2026-04-09 01:50:12 +0500

    behind the  scene

---
+++ BEHIND THE SCENE/app.js
@@ -0,0 +1,52 @@
+// function counter(){
+//     let count = 0
+
+//     return function(){
+//         count++
+//         console.log(count)
+//     }
+// }
+
+// let increment = counter()
+
+// increment()
+// increment()
+// increment()
+// increment()
+
+// function createMultiplier(){
+
+//     return function(){
+//        let multiply=  (12 *12)
+//        console.log(multiply)
+//     }
+// }
+
+// let result = createMultiplier()
+
+// result()
+
+// function counter(start){
+//     let count = 0
+
+//     function inc(){
+//         return count++
+//     }
+// }
+// let c = counter(6)
+
+// console.log(c)
+
+// function counter(start) {
+//     let count = start;
+
+//     return function () {
+//        return ++count
+//     }
+
+// }
+// let c = counter(5)
+// console.log(c())
+// console.log(c())
+// console.log(c())
+
