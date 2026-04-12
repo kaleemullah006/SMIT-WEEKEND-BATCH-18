704f271246765140f71873a4b596f96b7a7c08f7
Kaleem Ullah <kohatiii006@gmail.com>
2026-04-12 12:42:57 +0500

    typescript

---
+++ TYPESCRIPT/index.ts
@@ -0,0 +1,37 @@
+// let Name: string = "kaleem";
+// let Age: number = 20;
+// let isStudent: boolean = true;
+
+// console.log(Name);
+// console.log(Age);
+// console.log(isStudent);
+
+// let arr:number[]= [1,2,3];
+// console.log(arr)
+
+// function multiply(a:number,b:number):number{
+//  return a * b
+// }
+
+// console.log(multiply(6,6))
+
+// let myCar : {name:string, age: number} = {
+// name:"ali",
+// age : 20
+
+// }
+// console.log(myCar.age)
+
+// let car:{brand:string, model:string, year : number} = {
+//     brand: "Honda",
+//     model: "cd267",
+//     year: 2024
+// }
+// console.log(car.brand)
+
+// let id: string | number = true
+
+// console.log(id)
+
+
+
